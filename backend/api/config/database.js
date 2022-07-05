f98353f3af5a570d037b320ea8cfc7cee2a44000
Thevindu_Senanayake <thevindusenanayake76@gmail.com>
2022-07-05 18:24:54 +0530

    Connected to Online DB #3

--- backend/api/config/database.js
+++ backend/api/config/database.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-	mongoose.connect(process.env.DB_LOCAL_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	}).then(con => {
-		console.log(`MongoDB Database connected with host: ${con.connection.host}`)
-	}).catch(error => {
-		console.log(`Failed to connect mongoDB \n ${error}`);
-	})
-}
+	mongoose
+		.connect(process.env.DB_REMOTE_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then((con) => {
+			console.log(
+				`MongoDB Database connected with host: ${con.connection.host}`
+			);
+		})
+		.catch((error) => {
+			console.log(`Failed to connect mongoDB \n ${error}`);
+		});
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
