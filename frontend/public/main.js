5a5931e967d86f333a4031061c6e01837b6d1585
Danidu_Herath <kumudunaram@gmail.com>
2022-12-25 23:43:57 +0530

    icon added

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -4,38 +4,39 @@ const path = require("path");
 require("@electron/remote/main").initialize();
 
 function createWindow() {
-	// Create the browser window.
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true,
-			preload: path.join(__dirname, "preload.js"),
-		},
-	});
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: "icons/icon.ico",
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-	win.loadURL("http://localhost:3000");
+  win.loadURL("http://localhost:3000");
 }
 
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function () {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", function () {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // notification
 ipcMain.on("notify", (event, message) => {
-	new Notification({ title: "New Message", body: message }).show();
+  new Notification({ title: "New Message", body: message }).show();
 });
