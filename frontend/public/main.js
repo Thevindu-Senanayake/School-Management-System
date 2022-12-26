57e29af8348bae9175f076d5ccfcc1a67c13d07d
danidu2herath <intelherath@gmail.com>
2022-12-26 23:32:12 +0530

    App Icon Added

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: "icons/icon.ico",
+    icon: path.join(__dirname, "icons/icon.png"),
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
