011326e93996ce80cafcb59cf7cede3e19d18f94
Danidu_Herath <kumudunaram@gmail.com>
2022-12-27 03:32:59 +0530

    App Icon Updated

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, "icons/icon.png"),
+    icon: path.join(__dirname, "icons/icon .png"),
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
